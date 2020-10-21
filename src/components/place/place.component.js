a58fd0842caafbf260911eae4a1fb1b60f911004
Luan Bui <luanb3455@gmail.com>
2020-10-21 18:22:10 +0800

    places components and styles

---
+++ src/components/place/place.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./place-style.scss";
+
+const Place = (place) => (
+  <div className="place-container">
+    <h1 className="title">Place Item</h1>
+  </div>
+);
+
+export default Place;
---
+++ src/components/places-carousel/places-carousel.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./places-carousel-style.scss";
+
+const PlacesCarousel = (places) => (
+  <div className="places-carousel-container">
+    <h1 className="title">Places carousel</h1>
+  </div>
+);
+
+export default PlacesCarousel;
