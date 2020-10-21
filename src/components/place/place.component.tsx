bb905434da43679a742b604dc2d8c2aa6fddac7a
Luan Bui <luanb3455@gmail.com>
2020-10-22 00:50:22 +0800

    upgraded to material cards data

--- src/components/place/place.component.tsx
+++ src/components/place/place.component.tsx
@@ -1,4 +1,12 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 import "./place-style.scss";
 
@@ -8,14 +16,35 @@ interface IProps {
   location: string;
 }
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
 const Place = (props: IProps) => (
   <div className="place-container">
 
-    <img src={props.img} alt="image of location" width="250" height="100" />
+    <Card className="card-place">
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image={props.img}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.location}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
 
-
-    <h1 className="title">{props.title}</h1>
-    <p>{props.location}</p>
+    </Card>
   </div >
 );
 
